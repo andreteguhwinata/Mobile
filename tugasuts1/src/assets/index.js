7d395fa6049931831ce82759b8a2b2e595ec713d
andreteguhwinata <60992576+andreteguhwinata@users.noreply.github.com>
2020-11-29 14:32:38 +0800

    uts
    tugas uts

--- tugasuts1/src/assets/index.js
+++
@@ -1,5 +0,0 @@
-import BgSplash from './Splash/bgsplash_phonepedia.png';
-import AkunIlustration from './Ilustration/login.png';
-import Under from './Under/25509.jpg';
-
-export { BgSplash, AkunIlustration, Under };
--- tugasuts1/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-// disini tempat mengumpulkan semua page
-import Splash from './Splash';
-import Login from './Login';
-import Signup from './Signup';
-import BottomTab from './BottomTab/tabnavigation';
-
-export { Splash, Login, Signup, BottomTab };
